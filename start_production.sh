bd83ddb58cd1db905395b5702dfdb5d408fb7cee
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2021-06-14 15:50:26 -0400

    Bump version

--- start_production.sh
+++ start_production.sh
@@ -5,7 +5,7 @@ set -v
 # Docker image is pinned here, so that you can checkout older
 # versions of this script, and get reproducible deployments.
 # DOCKER_VERSION is the version of 4dndcic/higlass-docker
-DOCKER_VERSION=v0.0.5
+DOCKER_VERSION=v0.0.6
 IMAGE=4dndcic/higlass-docker:$DOCKER_VERSION
 STAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 PORT=80
